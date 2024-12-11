650f7e4178de6a2b7b84e0a42ba4705d24df4946
rk11machaba <68669221+rk11machaba@users.noreply.github.com>
2024-12-11 21:51:37 +0200

    update resume

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // You can add interactivity here, e.g., form submission, animations, etc.
-    const form = document.querySelector('form');
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert("Message sent!");
-        form.reset();
-    });
-});
-
-const hamburger = document.getElementById('hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', function () {
-    const hamIcon = this.querySelector('.hamburger-icon');
-    const crossIcon = this.querySelector('.cross-icon');
-    if (hamIcon.style.display === "none") {
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    }
-    else {
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
-    }
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
